b3c10b7309606a3a2766a1b0b94fd53084eb1a86
hanabi95 <youngsoo.cha@gmail.com>
2020-11-06 16:40:17 +0900

    Update steer_smoother.py

--- selfdrive/controls/lib/steer_smoother.py
+++ selfdrive/controls/lib/steer_smoother.py
@@ -1,7 +1,7 @@
 class SteerSmoother():
   def __init__(self):
-    self.data_rst = 0
-    self.data_steer = [0.0, 0.0]​
+    self.data_rst = 0.0
+    self.data_steer = [0.0, 0.0]
 
 # steer_angle_des smoother
   def get_data(self, steer_angle_dest, stAngle_Diff, reduceRate):
@@ -12,5 +12,5 @@ class SteerSmoother():
       self.data_steer[1] = self.data_rst #self.data_rst 값을 취함에 주의
     else:
       self.data_rst = steer_angle_dest
-      
+
     return self.data_rst
