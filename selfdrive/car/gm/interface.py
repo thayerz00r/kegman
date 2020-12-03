fdf56d6c69b80d6c1321e9ea39d809f9d71c8e0c
hanabi95 <youngsoo.cha@gmail.com>
2020-12-03 18:16:20 +0900

    fix

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -187,8 +187,6 @@ class CarInterface(CarInterfaceBase):
     ret.canValid = self.cp.can_valid
     ret.steeringRateLimited = self.CC.steer_rate_limited if self.CC is not None else False
 
-    ret.engineRPM = self.CS.engineRPM
-
     buttonEvents = []
 
     if self.CS.main_on:
