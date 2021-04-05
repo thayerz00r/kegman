790a1f50629c96398ebb0608944aa02dca7c824d
Thayer Merritt <thayer.merritt@gmail.com>
2021-04-05 15:15:49 -0700

    added a prereq install file

---
+++ tools/ubuntu_install_prereq.sh
@@ -0,0 +1,53 @@
+#added this to install prereqs before running the python setup portion.. speed things up after clone of disk image
+
+sudo apt-get update && sudo apt-get install -y --no-install-recommends \
+    autoconf \
+    build-essential \
+    bzip2 \
+    capnproto \
+    cppcheck \
+    libcapnp-dev \
+    clang \
+    cmake \
+    curl \
+    ffmpeg \
+    git \
+    libavformat-dev libavcodec-dev libavdevice-dev libavutil-dev libswscale-dev libavresample-dev libavfilter-dev \
+    libarchive-dev \
+    libbz2-dev \
+    libcurl4-openssl-dev \
+    libeigen3-dev \
+    libffi-dev \
+    libglew-dev \
+    libgles2-mesa-dev \
+    libglfw3-dev \
+    libglib2.0-0 \
+    liblzma-dev \
+    libmysqlclient-dev \
+    libomp-dev \
+    libopencv-dev \
+    libpng16-16 \
+    libssl-dev \
+    libstdc++-arm-none-eabi-newlib \
+    libsqlite3-dev \
+    libtool \
+    libusb-1.0-0-dev \
+    libzmq3-dev \
+    libsdl-image1.2-dev libsdl-mixer1.2-dev libsdl-ttf2.0-dev libsmpeg-dev \
+    libsdl1.2-dev  libportmidi-dev libswscale-dev libavformat-dev libavcodec-dev libfreetype6-dev \
+    libsystemd-dev \
+    locales \
+    ocl-icd-libopencl1 \
+    ocl-icd-opencl-dev \
+    opencl-headers \
+    python-dev \
+    python3-pip \
+    qml-module-qtquick2 \
+    qt5-default \
+    qtmultimedia5-dev \
+    qtwebengine5-dev \
+    screen \
+    sudo \
+    vim \
+    wget \
+    gcc-arm-none-eabi
